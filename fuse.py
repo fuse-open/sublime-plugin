d44f1e5b8a68c8297f3600e500482a42260b6ab0
Andreas Rønning <andreas@fusetools.com>
2015-08-07 19:37:37 +0200

    Update changelog Add fuse create options for js and json Add menu option for sublime use

--- fuse.py
+++ fuse.py
@@ -333,6 +333,7 @@ class FuseBuild(sublime_plugin.WindowCommand):
 
 		subprocess.Popen(gFuse.previousBuildCommand, cwd=working_dir, shell=True)
 
+
 class FuseCreate(sublime_plugin.WindowCommand):
 	targetFolder = ""
 	targetTemplate = ""
@@ -356,12 +357,12 @@ class FuseCreate(sublime_plugin.WindowCommand):
 
 
 		header = "";
-		if type=="ux":
-			header = "Choose a file name:"
+		if type=="app":
+			header = "Choose a project name:"
 		elif type=="uno":
 			header = "Choose a class name:"
 		else:
-			header = "Choose a project name:"
+			header = "Choose a file name:"
 
 		self.window.show_input_panel(header, "", self.on_done, None, None)
 
