a1cd3feaf3de37938cb505d02a7f3466d8e8de90
Anders Schau Knatten <anders@knatten.org>
2015-11-03 13:50:34 +0100

    Added release script

---
+++ release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+if [ $# != 4 ]; then
+    echo "USAGE release.sh <zip file name> <token> <upload url> <redirect url>"
+    exit 1
+fi
+
+RELEASE_FILE=$1
+TOKEN=$2
+UPLOAD_URL=$3
+REDIRECT_URL=$4
+
+REDIRECT="" #TODO use "latest-sublime-plugin-2"
+
+echo "Uploading '$RELEASE_FILE' to '$UPLOAD_URL'"
+UPLOAD_CMD="curl -H 'X-API-Token: $TOKEN' -F \"file=@$RELEASE_FILE\" $UPLOAD_URL"
+echo $UPLOAD_CMD
+OUTPUT=$(eval $UPLOAD_CMD)
+echo
+echo $OUTPUT
+
+URL=$(echo $OUTPUT | sed 's/.*"url":"\(.*\)".*/\1/')
+echo
+echo "Setting '$REDIRECT' to point to '$URL'"
+
+PAYLOAD='{"uri":"'$URL'"}'
+echo
+echo "Payload is '$PAYLOAD'"
+
+REDIRECT_CMD="curl -H 'Content-Type: application/json' -H 'X-API-Token: $TOKEN' -X PUT -d '$PAYLOAD' $REDIRECT_URL/$REDIRECT"
+echo $REDIRECT_CMD
+eval $REDIRECT_CMD
+
+echo
+echo "##teamcity[setParameter name='releaseVersion' value='$(cat VERSION.txt)']"
