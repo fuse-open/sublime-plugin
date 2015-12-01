c2481f5f0a858880407eb0eb60e2b45c4bf1ef9d
Anders Schau Knatten <anders@knatten.org>
2015-12-01 11:05:11 +0100

    Ensure log directory exists

--- fuse.py
+++ fuse.py
@@ -249,15 +249,27 @@ def userdata_dir():
 	else:
 		return os.path.join(os.getenv("LOCALAPPDATA"), "Fusetools", "Fuse")
 
+def log_dir():
+	return os.path.join(userdata_dir(), "logs")
+
 def log_file():
-	return os.path.join(userdata_dir(), "logs", "fuse.sublimeplugin.log")
+	return os.path.join(log_dir(), "fuse.sublimeplugin.log")
+
+def ensure_dir_exists(dir):
+	try:
+		os.makedirs(dir)
+	except FileExistsError:
+		return
+	except Exception as e:
+		sublime.error_message("Could not create directory '" + dir + "'. Please make sure you have the correct permissions: " + str(e))
 
 def configure_logging():
 	log = logging.getLogger(__name__.split(".")[0])
-	if (len(log.handlers) > 0):
+	if (len(log.handlers) > 0): #Already configured
 		return
 
 	print("logging to " + log_file())
+	ensure_dir_exists(log_dir())
 	handler = logging.handlers.RotatingFileHandler(log_file(), 'a', 500000, 5, "utf-8")
 	formatter = logging.Formatter('%(asctime)s [%(process)d] %(levelname)s %(message)s')
 	handler.setFormatter(formatter)
